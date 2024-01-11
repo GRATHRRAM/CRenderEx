569d6cadc15e2b085a7035927ee76039c29acfb6
GRATHRRAM <101575465+scriptguy1@users.noreply.github.com>
2024-01-11 20:27:17 +0100

    Add files via upload

--- CRenderEx.h
+++ CRenderEx.h
@@ -27,22 +27,24 @@ typedef struct CR_Rect {
     uint32_t y;
     uint32_t Height;
     uint32_t Width;
-    char Fill;
+    char Char;
 } CR_Rect;
 
 //If No Errors Returns 0 Else errsig (All Functions!!! if not void)
+//If you see any error please fix or report. Thanks!!!
 uint8_t CR_AllocRender  (CR_Render *Render, uint32_t ResolutionX, uint32_t ResolutionY); //Allocs space on heap for Render
 uint8_t CR_ReallocRender(CR_Render *Render, uint32_t ResolutionX, uint32_t ResolutionY); //Changes Resolution For Already Existing Render
 
 void CR_RenderFill (CR_Render *Render, char Character); //Fills Render With Characters
 void CR_RenderPrint(CR_Render Render);                  //Prints Graphics/Display You know what i mean
 
-uint8_t CR_RenderSetChar(CR_Render *Render, uint32_t PositionX, uint32_t PositionY,  char Character);//Replace Character at g
+uint8_t CR_RenderSetChar(CR_Render *Render, uint32_t PositionX, uint32_t PositionY,  char Character);//Replace Character at given position
+
+uint8_t CR_Rect2Render(CR_Render *Render, CR_Rect Rect);//Overwrites render with rect
 
 void CR_GetErrDesc(uint8_t Error); //prints description of error
 
 
-//If you see any error please fix or report. Thanks!!!
 uint8_t CR_AllocRender(CR_Render *Render, uint32_t ResolutionX, uint32_t ResolutionY) {
     Render->Chars = (char**) malloc(sizeof(char*) * ResolutionY);
     for(uint32_t i=0; i < ResolutionY; ++i)
@@ -89,6 +91,15 @@ uint8_t CR_RenderSetChar(CR_Render *Render, uint32_t PositionX, uint32_t Positio
         return SIGOUTB;
     else Render->Chars[PositionY][PositionX] = Character;
     return SIGNONE;
+}
+
+uint8_t CR_Rect2Render(CR_Render *Render, CR_Rect Rect) {
+    uint8_t outofborders = 0;
+    for(uint32_t y = Rect.y; y < Rect.Height; ++y) {
+        for(uint32_t x = Rect.x; x < Rect.Width; ++x) {
+            CR_RenderSetChar(Render, x, y, Rect.Char);
+        }
+    }
 }
 
 void CR_GetErrDesc(uint8_t Error) {
